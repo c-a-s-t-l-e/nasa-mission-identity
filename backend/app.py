169a1163c3cdf476f574b59b6a7b37cf246b2fa6
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2023-11-17 10:26:02 -0500

    descriptions added and allows users to enter their own abstract

--- backend/app.py
+++ backend/app.py
@@ -1,32 +1,47 @@
 from flask import Flask, request
 from flask_cors import CORS
-from data_processing import get_missions
+from data_processing import get_missions, get_missions_csv
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
 
-# @app.get('/')
-# def retrieve_data():
-#    return get_missions()
 
-# endpoint allows user to upload files
-# file is then passed on to the get_missions func in data_processing.py 
-@app.post('/upload')
-def upload_file():
-   #  get file from req body 
-    if 'file' not in request.files:
-        return "No file uploaded!"
-    print('file successfully selected')
-    file = request.files['file']
+# endpoint allows users to submit a single abstract
+# abstract is then passed to get_missions as a dataframe
+@app.post("/submit-abstract")
+def submit_abstract():
+    abstract_text = request.json.get("abstractText")
+    if not abstract_text:
+        return "No abstract text provided!", 400
 
-    # Check if the uploaded file is a CSV
-    if not file.filename.endswith('.csv'):
-        return "Invalid file type! Please upload a CSV."
+    # create a DataFrame with necessary columns for a single abstract
+    abstract_df = pd.DataFrame(
+        {
+            "ABSTRACT": [abstract_text],
+            "ABSTRACT_NUM": [0],
+        }
+    )
 
-    print('sending csv to data processing')
-    missions = get_missions(file)
+    # Call get_missions with DataFrame instead of file
+    missions = get_missions(abstract_df)
     return missions
 
-# TODO 
-# CREATE FRONTEND MATCHING THE IR STYLING ON FIGMA / CANVA 
-# USE REACT TO IMPLEMENT DESIGN
+
+# endpoint allows user to upload files
+# file is then passed on to the get_missions func in data_processing.py
+@app.post("/upload")
+def upload_file():
+    #  get file from req body
+    if "file" not in request.files:
+        return "No file uploaded!"
+    print("file successfully selected")
+    file = request.files["file"]
+
+    # check if the uploaded file is a CSV
+    if not file.filename.endswith(".csv"):
+        return "Invalid file type! Please upload a CSV."
+
+    print("sending csv to data processing")
+    missions = get_missions_csv(file)
+    return missions
--- backend/data_processing.py
+++ backend/data_processing.py
@@ -4,29 +4,35 @@ from spacy.matcher import PhraseMatcher
 from spacy.tokens import Span
 from flask import jsonify
 
-# take in file as parameter to allow users to upload their own abstracts
-def get_missions(file):
-    # read in the sheets seperately saved as csv's
 
-    review = pd.read_csv(file)
+# take in file as parameter to allow users to upload their own abstracts
+def get_missions(input):
     export = pd.read_csv("export_mission.csv")
+
+    # check if input is a DataFrame or a file path
+    if isinstance(input, pd.DataFrame):
+        # if DataFrame use it directly
+        review = input
+    else:
+        # read the csv uploaded
+        review = pd.read_csv(input)
 
     # reset the index column in the review sheet since the first two columns do not contain data
     # ABSTRACT_NUM will be used to merge to the results later
-    review["ABSTRACT_NUM"] = review.index-2
-
+    review["ABSTRACT_NUM"] = 0
+    print(review["ABSTRACT_NUM"])
     # move ABSTRACT_NUM to the start of the dataframe
-    first_column = review.pop('ABSTRACT_NUM')
-    review.insert(0, 'ABSTRACT_NUM', first_column)
+    first_column = review.pop("ABSTRACT_NUM")
+    review.insert(0, "ABSTRACT_NUM", first_column)
 
     # get the pid and name for each mission
-    pid_name = export[["PID", "NAME"]][2:]
-
+    pid_name = export[["PID", "NAME"]][0:]
     # grab the abstracts from the review csv
-    abstracts = pd.DataFrame(review["ABSTRACT"][2:])
+    # print(review["ABSTRACT"])
+    abstracts = pd.DataFrame(review["ABSTRACT"][0:])
 
     # reset the index values
-    abstracts = abstracts['ABSTRACT'].reset_index().drop(columns=['index'])
+    abstracts = abstracts["ABSTRACT"].reset_index().drop(columns=["index"])
 
     #########################################################################
 
@@ -51,7 +57,9 @@ def get_missions(file):
     docs = []
     for doc in doc_abs:
         matches = matcher(doc)
-        spans = [Span(doc, start, end, label=match_id) for match_id, start, end in matches]
+        spans = [
+            Span(doc, start, end, label=match_id) for match_id, start, end in matches
+        ]
         doc.ents = spacy.util.filter_spans(spans)
         docs.append(doc)
 
@@ -59,16 +67,16 @@ def get_missions(file):
     matches = [matcher(doc) for doc in doc_abs]
 
     # make an empty dataframe
-    doc_dataframe = pd.DataFrame(columns=["ABSTRACT_NUM","NAME"])
+    doc_dataframe = pd.DataFrame(columns=["ABSTRACT_NUM", "NAME"])
 
     # fill the dataframe with matches that exist in the abstracts
     count = 0
     for match_list in matches:
         for match_id, start, end in match_list:
             rule_id = nlp.vocab.strings[match_id]
-            span = doc_abs[count][start : end]
+            span = doc_abs[count][start:end]
             doc_dataframe.loc[len(doc_dataframe)] = [count, span.text]
-        count+=1
+        count += 1
 
     ######################################################################
 
@@ -85,7 +93,7 @@ def get_missions(file):
     doc_dataframe = doc_dataframe.merge(review, on="ABSTRACT_NUM")
 
     # Select specific columns
-    filtered_dataframe = doc_dataframe[['ABSTRACT_NUM', 'NAME', 'AUTO MISSION PID', 'TITLE', 'CITATION SOURCE']]
+    filtered_dataframe = doc_dataframe[["ABSTRACT_NUM", "NAME", "AUTO MISSION PID"]]
 
     # remove duplicates
     filtered_dataframe.drop_duplicates(inplace=True)
@@ -97,5 +105,105 @@ def get_missions(file):
     return filtered_dataframe.to_json()
 
 
+########################################## TEMPORARY FIX ##########################################
+# take in file as parameter to allow users to upload their own abstracts
+def get_missions_csv(file):
+    # read in the sheets seperately saved as csv's
 
+    review = pd.read_csv(file)
+    export = pd.read_csv("export_mission.csv")
 
+    # Add missing columns with default values if they don't exist
+    if "TITLE" not in review.columns:
+        review["TITLE"] = "Default Title"
+    if "CITATION SOURCE" not in review.columns:
+        review["CITATION SOURCE"] = "Default Citation Source"
+
+    # reset the index column in the review sheet since the first two columns do not contain data
+    # ABSTRACT_NUM will be used to merge to the results later
+    review["ABSTRACT_NUM"] = review.index
+
+    # move ABSTRACT_NUM to the start of the dataframe
+    first_column = review.pop("ABSTRACT_NUM")
+    review.insert(0, "ABSTRACT_NUM", first_column)
+
+    # get the pid and name for each mission
+    pid_name = export[["PID", "NAME"]][0:]
+
+    # grab the abstracts from the review csv
+    abstracts = pd.DataFrame(review["ABSTRACT"][0:])
+
+    # reset the index values
+    abstracts = abstracts["ABSTRACT"].reset_index().drop(columns=["index"])
+
+    #########################################################################
+
+    # load the pretrained model
+    nlp = spacy.load("en_core_web_sm")
+
+    # make a matcher object
+    matcher = PhraseMatcher(nlp.vocab)
+
+    # add the patterns from the "NAME" column in the pid_name dataframe to the matcher object
+    patterns = [nlp.make_doc(name) for name in pid_name["NAME"]]
+    matcher.add("NAME", patterns)
+
+    # make each abstract a document object to be processed, saved in the doc_abs list
+    doc_abs = [nlp.make_doc(abstract) for abstract in abstracts["ABSTRACT"].apply(str)]
+
+    # How this code works:
+    # 1. For each document object in doc_abs, find a match if there is one by making the match a Span object
+    # 2. For each Span object in a document add it to the entities attribute of the Document object
+    # 3. Add the document with its updated list of entities to the docs list
+
+    docs = []
+    for doc in doc_abs:
+        matches = matcher(doc)
+        spans = [
+            Span(doc, start, end, label=match_id) for match_id, start, end in matches
+        ]
+        doc.ents = spacy.util.filter_spans(spans)
+        docs.append(doc)
+
+    # get the matches in each abstract
+    matches = [matcher(doc) for doc in doc_abs]
+
+    # make an empty dataframe
+    doc_dataframe = pd.DataFrame(columns=["ABSTRACT_NUM", "NAME"])
+
+    # fill the dataframe with matches that exist in the abstracts
+    count = 0
+    for match_list in matches:
+        for match_id, start, end in match_list:
+            rule_id = nlp.vocab.strings[match_id]
+            span = doc_abs[count][start:end]
+            doc_dataframe.loc[len(doc_dataframe)] = [count, span.text]
+        count += 1
+
+    ######################################################################
+
+    # merge the mission names with their respective pid's,
+    doc_dataframe = doc_dataframe.merge(pid_name, on="NAME")
+
+    # rename the PID column to MISSION PID
+    doc_dataframe = doc_dataframe.rename(columns={"PID": "AUTO MISSION PID"})
+
+    # drop the columns after the ABSTRACT
+    review.drop(review.iloc[:, 10:], inplace=True, axis=1)
+
+    # merge the review csv with the ABSTRACT_NUM column
+    doc_dataframe = doc_dataframe.merge(review, on="ABSTRACT_NUM")
+
+    # Select specific columns
+    filtered_dataframe = doc_dataframe[
+        ["ABSTRACT_NUM", "NAME", "AUTO MISSION PID", "TITLE", "CITATION SOURCE"]
+    ]
+
+    # remove duplicates
+    filtered_dataframe.drop_duplicates(inplace=True)
+
+    # create a new LINK column and set the results to "Y"
+    doc_dataframe["LINK"] = "Y"
+
+    # returns all matches and converts it to JSON
+    return filtered_dataframe.to_json()
